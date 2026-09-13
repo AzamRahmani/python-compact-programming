068bf4014924f85fc2d226218b6239a680d3e852
Azam Rahmani <Rahmani.aazam13@gmail.com>
2026-09-13 16:51:32 +0200

    Implement circle area calculator

--- week_01/main.py
+++ week_01/main.py
@@ -1,2 +1,6 @@
-print("Hello, Azam!")
-print("Welcome to the Python Compact Programming Course.")
+import math
+
+radius = float(input("Enter the circle radius: "))
+area = math.pi * radius ** 2
+
+print("The circle area is:", area)
