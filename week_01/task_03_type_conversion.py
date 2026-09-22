09c2887624d0564e7206f8d0a620e29fa3a2f6ad
Azam Rahmani <rahmani.aazam13@gmail.com>
2026-09-22 11:12:03 +0200

    Complete type conversion task

---
+++ week_01/task_03_type_conversion.py
@@ -0,0 +1,16 @@
+zahl = 10
+kommazahl = 10.5
+text = "123"
+
+zahl_als_float = float(zahl)
+kommazahl_als_int = int(kommazahl)
+text_als_int = int(text)
+
+print("Integer converted to float:", zahl_als_float)
+print("Type:", type(zahl_als_float))
+
+print("Float converted to integer:", kommazahl_als_int)
+print("Type:", type(kommazahl_als_int))
+
+print("String converted to integer:", text_als_int)
+print("Type:", type(text_als_int))
